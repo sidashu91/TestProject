69effe67c62f6eeb8145f2aa0589fe803dcb476b
Siddharth Chaturvedi <57729432+sidashu91@users.noreply.github.com>
2020-10-08 14:08:00 +0530

    Update Find_Duplicate_Chars_In_A_String.java

--- JavaCodes/src/stringHandling/Find_Duplicate_Chars_In_A_String.java
+++ JavaCodes/src/stringHandling/Find_Duplicate_Chars_In_A_String.java
@@ -1,31 +1,25 @@
-package stringHandling;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class Find_Duplicate_Chars_In_A_String {
-
-	public static void main(String[] args) {
-		String blogName = "howtodoinjava dot com";
-		char[] chars = blogName.toCharArray();
-
-		Map<Character, Integer> map = new HashMap<>();
-		for (int i = 0; i < chars.length; i++) {
-			System.out.println(chars[i]);
-			if (map.containsKey(chars[i])) {
-				int counter = map.get(chars[i]);
-				System.out.println(counter);
-				map.put(chars[i], ++counter);
-			}
-			
-			else {
-				map.put(chars[i], 1);
-			}
-			{
-			}
-
-		}
-
-	}
-
-}
+public class Find_Duplicate_Chars_In_A_String {  
+     public static void main(String[] args) {  
+        String string1 = "Great responsibility";  
+        int count;  
+          
+        //Converts given string into character array  
+        char string[] = string1.toCharArray();  
+          
+        System.out.println("Duplicate characters in a given string: ");  
+        //Counts each character present in the string  
+        for(int i = 0; i <string.length; i++) {  
+            count = 1;  
+            for(int j = i+1; j <string.length; j++) {  
+                if(string[i] == string[j] && string[i] != ' ') {  
+                    count++;  
+                    //Set string[j] to 0 to avoid printing visited character  
+                    string[j] = '0';  
+                }  
+            }  
+            //A character is considered as duplicate if count is greater than 1  
+            if(count > 1 && string[i] != '0')  
+                System.out.println(string[i]);  
+        }  
+    }  
+}  
