e3e29c5be521b17757918478ffc374f176764887
Siddharth Chaturvedi <siddhartha.chaturvedi91@gmail.com>
2020-10-13 11:33:59 +0530

    Commit

--- JavaCodes/src/abstractClassImplementation/A.java
+++ JavaCodes/src/abstractClassImplementation/A.java
@@ -2,6 +2,12 @@ package abstractClassImplementation;
 
 public abstract class A {
 	
+	//Abstract Classes and Methods Data 
+	//abstraction is the process of hiding certain details and
+	//showing only essential information to the user. 
+	//Abstraction can be achieved with either abstract classes or interfaces (which you will learn more about in the next chapter).
+	
+	
 	
 	abstract void msg();//abstract menthod1
 
---
+++ JavaCodes/src/basicPrograms/Array_PyramidPattern.java
@@ -0,0 +1,27 @@
+package basicPrograms;
+
+public class Array_PyramidPattern {
+
+	public static void main(String[] args) {
+		
+		
+		int n=5;
+	
+		for (int i = 0; i < n; i++) {
+			
+			System.out.println(" ");
+			
+			
+			for (int j = 0; j <i; j++) {
+				
+				System.out.println("* ");
+				
+			}
+			System.out.println();
+			
+		}
+		
+
+	}
+
+}
